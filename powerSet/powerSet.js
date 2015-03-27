f2972696358f2fa07552d5f4ebf62a24910e542e
Janessa Link <janessa@gmail.com>
2015-03-27 08:41:23 -0700

    Updated PS

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -50,3 +50,30 @@ var powerSet = function(str){
 
 var test1 = "OMG";
 powerSet(test1);
+
+
+//Solution- still needs to add the full unique string
+// var powerSet = function(str){
+//   str = str || '';
+//   var letters = {};
+//   var solutions = {};
+
+//   for(var i = 0; i < str.length; i++){
+//     letters[str[i]] = true;
+//   }
+
+//   str = Object.keys(letters).join(‘’);
+
+//   var recurse = function(strSet){
+//     for(var i = 0; i < strSet.length; i++) {
+//       var subSet = strSet.substr(0, i) + strSet.substr(i+1, strSet.length);
+//       if (!solutions[subSet]) {
+//         solutions[subSet] = true;
+//         recurse(subSet);
+//       }
+//     }
+//   };
+
+//   recurse(str);
+//   return Object.keys(solutions);
+// };
