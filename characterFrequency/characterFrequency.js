05cb02a850869a5a9fb3be0c52ec77030ae948f9
Janessa Link <janessa@gmail.com>
2015-05-05 09:50:52 -0700

    [completed] Completed characterFrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -35,7 +35,58 @@
  *
  */
 
-
 var characterFrequency = function(string) {
+  var charFreq = {};
+  var result = [];
+
+  for(var i = 0; i < string.length; i++){
+    //Load the CharFreq object
+    if( charFreq[string[i]] ){
+      charFreq[string[i]]++;
+    }else{
+      charFreq[string[i]] = 1;
+    }
+    //print("Loaded frequence object: ", charFreq);
+  }
+
+  // var allKeys = Object.keys(charFreq);
+  // allKeys.sort();
+  // print("All Keys: ", allKeys)
+
+  for (var keys in charFreq){
+    var tempArr = [];
+    tempArr.push(keys);
+    tempArr.push(charFreq[keys]);
+    //print("TempArrays: ", tempArr);
+    result.push(tempArr);
+
+  }
+  //print("Before sort ", result);
+  //Now need to sort
+  //Know result in form of [ [a,4], [b,2] ... ];
+
+  for(var i = 0; i < result.length; i++){
+    //Modified insertion sort from yesterday's toy prob
+    var tempValue = result[i][1];
+    for(var j = i+1; j < result.length; j++){
+      if (result[i][1] < result[j][1] ){
+        //print("Found a case to swap! " , result[i][1], " at ", i, " with ", result[j][1], " at ", j);
+        //Swap in place. Already have array[i] saved as tempValue so can overwrite in array
+        result[i][1] = result[j][1];
+        result[j][1] = tempValue;
+      }
+    }
+    //print("After sorting ", result);
+
+  }
+
   return result;
-};
+}; //tested, working
+
+//Basic testing
+//characterFrequency("bbaaac");
+//characterFrequency("abcdddead");
+
+
+
+
