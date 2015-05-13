8438a8d401ddfe1ad7f170dd2fc01b27b6e500e0
Janessa Link <janessa@gmail.com>
2015-05-13 09:26:10 -0700

    [add] coinSums attempted

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -1,6 +1,7 @@
 /*
 
-In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+In England the currency is made up of pound, £, and pence, p, and there are eight coins in
+general circulation:
 
 1p piece
 2p piece
@@ -24,8 +25,46 @@ makeChange(1) === 1
 makeChange(2) === 2
 */
 
-var makeChange = function(total){
+var changeUK = {
+  "1p": 1,
+  "2p": 2,
+  "5p": 5,
+  "10p": 10,
+  "20p": 20,
+  "50p": 50,
+  "euro": 100
+};
+
+var makeChange = function(total){ //total is a value in whole pence
+  var results = [];
+  var tempChange = [];
+  var runningRemainder = total;
+  var orderedCoins = ["euro", "50p", "20p", "10p", "5p", "2p", "1p"];
+
+// Finds first, naive solution. Doesn't repeat coins (ie 4 = 2+2)
+
+
+    while (runningRemainder>0){
+      for(var i =0; i<orderedCoins.length; i++){
+        //print("Tring to fit ", orderedCoins[i], " into change for ", runningRemainder);
+        if(runningRemainder - changeUK[orderedCoins[i]] >=0){
+          //print("Change can be made: ", runningRemainder, " - ", changeUK[orderedCoins[i]])
+          tempChange.push(orderedCoins[i]);
+          runningRemainder = runningRemainder- changeUK[orderedCoins[i]];
+          //print("Added changed ", orderedCoins[i], " remaing change to give out: ", runningRemainder);
+        }
+
+
+    }
+    //print("Change: " , tempChange)
+    results.push(tempChange);
+  }
+
+  // Start next tempChange solution by skipping past first element in last array solution
+  // (ie if largest coin denomination that fit into last example was 5, skip that)
 
 };
+
+makeChange(4);
 
 
