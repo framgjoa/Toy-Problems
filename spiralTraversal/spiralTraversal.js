4d9c4e89acc9eccf501529a69b34647f14ee2437
Janessa Link <janessa@gmail.com>
2015-05-27 11:01:02 -0700

    [attempted] Spiral Traversal attempted

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -15,6 +15,32 @@
  */
 
 var spiralTraversal = function(matrix){
+  var colLength = matrix.length;
+  var rowLength = matrix[0].length;
+  // Can know how far spiraled in based on length of results
+  // Final results will be done when results.length = colLength * rowLength
+  var results = [];
+  // We can know "where" we are in the spiral based on length of results, too
+  // [1, 2, 3, 6, 9, 8, 7, 4, 5]
+  // firstRow (rowLength) -> last col (colLength -1) -> last row (rowLength-1) ->
+  // inside col (col -1) -> inside row(rowlength-2)
+  // Elements per direction: 3, 2, 2, 2, 1
+  // Directions:             R, D, L, U, R
 
-  // TODO: Implement me!
+  // A 2x2 would be: 2, 1, 1
+  // A 4x4 would be: 4, 3, 3, 2, 2, 2, 2
+  // A 5x5 would be: 5, 4, 4, 3, 3, 2, 2, 2
+
+
+  var matrixSpiral = function(matrix, startCol, startRow, direction){
+
+  };
+
+  var goRight = function(matrix, startCol, startRow){};
+  var goLeft = function(matrix, startCol, startRow){};
+  var goDown = function(matrix, startCol, startRow){};
+  var goUp = function(matrix, startCol, startRow){};
+
+
+
 };
