02d069e1b15950ee130fa53d8c2cf97afc47ce0f
Janessa Link <janessa@gmail.com>
2015-07-02 16:45:38 -0700

    powerSet updated

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -17,63 +17,28 @@
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
+
 var powerSet = function(str){
-  var results = [];
-  var frontCounter = 0;
-  var rearCounter = 0;
-  var base = "";
+  str = str || '';
+  var letters = {};
+  var solutions = {};
 
-  var splitString = str.split("");
-  results.push(splitString);
-
-  var pushAllThings = function(){
-    for(var j = 0; j < str.length; j++){
-      var base = str[j]| "";
-
-      if(base === str){       //base break out of recursion case,
-        results.push(base);   //when string is fully built up, still needs to be added to powerset
-        return;
-      }
-
-      base = base.concat(str[j+1]);
-    }
+  for(var i = 0; i < str.length; i++){
+    letters[str[i]] = true;
   }
 
-  pushAllThings(str);
+  str = Object.keys(letters).join(‘’);
 
-  print(results);
-  return results;
+  var recurse = function(strSet){
+    for(var i = 0; i < strSet.length; i++) {
+      var subSet = strSet.substr(0, i) + strSet.substr(i+1, strSet.length);
+      if (!solutions[subSet]) {
+        solutions[subSet] = true;
+        recurse(subSet);
+      }
+    }
+  };
 
-
-
+  recurse(str);
+  return Object.keys(solutions);
 };
-
-var test1 = "OMG";
-powerSet(test1);
-
-
-//Solution- still needs to add the full unique string
-// var powerSet = function(str){
-//   str = str || '';
-//   var letters = {};
-//   var solutions = {};
-
-//   for(var i = 0; i < str.length; i++){
-//     letters[str[i]] = true;
-//   }
-
-//   str = Object.keys(letters).join(‘’);
-
-//   var recurse = function(strSet){
-//     for(var i = 0; i < strSet.length; i++) {
-//       var subSet = strSet.substr(0, i) + strSet.substr(i+1, strSet.length);
-//       if (!solutions[subSet]) {
-//         solutions[subSet] = true;
-//         recurse(subSet);
-//       }
-//     }
-//   };
-
-//   recurse(str);
-//   return Object.keys(solutions);
-// };
