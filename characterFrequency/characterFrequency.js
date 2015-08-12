d318bff729accb3c993eb1c5ec37826e39e3857f
Janessa Link <janessa@gmail.com>
2015-08-12 09:16:37 -0700

    Improved sort using JS native

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -46,42 +46,42 @@ var characterFrequency = function(string) {
     }else{
       charFreq[string[i]] = 1;
     }
-    //print("Loaded frequence object: ", charFreq);
   }
-
-  // var allKeys = Object.keys(charFreq);
-  // allKeys.sort();
-  // print("All Keys: ", allKeys)
 
   for (var keys in charFreq){
     var tempArr = [];
     tempArr.push(keys);
     tempArr.push(charFreq[keys]);
-    //print("TempArrays: ", tempArr);
     result.push(tempArr);
 
   }
-  //print("Before sort ", result);
-  //Now need to sort
-  //Know result in form of [ [a,4], [b,2] ... ];
 
-  for(var i = 0; i < result.length; i++){
-    //Modified insertion sort from yesterday's toy prob
-    var tempValue = result[i][1];
-    for(var j = i+1; j < result.length; j++){
-      if (result[i][1] < result[j][1] ){
-        //print("Found a case to swap! " , result[i][1], " at ", i, " with ", result[j][1], " at ", j);
-        //Swap in place. Already have array[i] saved as tempValue so can overwrite in array
-        result[i][1] = result[j][1];
-        result[j][1] = tempValue;
-      }
+  // Sort: Know result in form of [ [a,4], [b,2] ... ]
+  // So need to compare the second (last) value of each two-ple
+
+  result.sort(function(a,b){
+    if (a[1] > b[1]){
+      return -1;
+    }else if(a[1]<b[1]){
+      return 1;
     }
-    //print("After sorting ", result);
+  });
 
-  }
-
+  //Modified insertion sort from yesterday's toy prob
+  // for(var i = 0; i < result.length; i++){
+  //   var tempValue = result[i][1];
+  //   for(var j = i+1; j < result.length; j++){
+  //     if (result[i][1] < result[j][1] ){
+  //       //print("Found a case to swap! " , result[i][1], " at ", i, " with ", result[j][1], " at ", j);
+  //       //Swap in place. Already have array[i] saved as tempValue so can overwrite in array
+  //       result[i][1] = result[j][1];
+  //       result[j][1] = tempValue;
+  //     }
+  //   }
+  // }
+  //print("After sorting ", result);
   return result;
-}; //tested, working
+};
 
 //Basic testing
 //characterFrequency("bbaaac");
---
+++ gibberishMaker/gibberishMaker.js
@@ -0,0 +1,36 @@
+var gibberishMaker = function(wordCount){
+
+};
+
+
+
+var gibberWord = function(){
+
+  var wordLength = Math.floor(8* Math.random()) + 1;
+
+  var consonantPatterns = [b,c,ck,ch,cd,cs,ct,cy,d,f,ft,g,gh,gt,gl,g,
+    h,j,k,l,ll,ln, lm,m,mn,n,ng,nh,p,pt,qu,r, rn, rm, rr,s,ss,st,t,v,w,x,y,z];
+  var vowelPatterns =[a,e,i,o,u,y,ae,ei,ou,yo,ay,oo,ee,eu,yi,ea];
+
+  var currentWord = '';
+  var currentWordLength = wordLength;
+  while(currentWordLength > 0){
+    if(currentWordLength ===1){
+
+    }
+
+
+  }
+
+
+
+};
+
+var gibberSentance = function(words){
+
+
+};
+
+var gibberParagraph = function(sentCount){
+
+};
